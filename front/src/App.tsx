71a1d086cfb44936254f4c4c9dfdb4da2f325134
lemanrza <lemaner@code.edu.az>
2025-07-16 02:47:32 +0400

    bug: redirecting in vercel

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ROUTES from "./routes/Route";
 import { Toaster } from "./components/ui/sonner";
 import "./i18n/config";
+
 const router = createBrowserRouter(ROUTES);
 
 function App() {
